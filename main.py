c79aaa276e6b1a75f095bcded563a51eea185d37
Tea <icepie.dev@gmail.com>
2020-03-27 01:55:53 +0800

    add the judgment for login

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-
-#_*_ coding:utf-8 _*_
+# _*_ coding:utf-8 _*_
 import requests
 import json
 import hashlib
 
-#login
-username = ""
+# data
+url = "http://hmgr.sec.lit.edu.cn/wms/healthyLogin"
+username = "" #input your username
 password = 123456
 
-#sha256 for password
-def get_sha256(data) :
+# sha256 for password
+def get_sha256(data):
     s1 = hashlib.sha256()
     s1.update(json.dumps(data).encode())
-    return  s1.hexdigest()
+    return s1.hexdigest()
 
-url="http://hmgr.sec.lit.edu.cn/wms/healthyLogin"
-
+# login
 headers = {
- 'Content-Type': 'application/json',
+    'Content-Type': 'application/json',
 }
 
 data = {
@@ -26,7 +25,10 @@ data = {
 }
 
 response = requests.post(url=url, data=json.dumps(data), headers=headers, verify=False)
-print(response.json())
 
-
-
+# print(response.json())
+if not 'True' in response.json():
+    print("登陆成功!")
+    print('学号: ' + response.json()['data']['teamNo'])
+else:
+    print("登陆失败! 请检查学号和密码.")
