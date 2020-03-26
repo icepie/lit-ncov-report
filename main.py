a399e0e7715bf7da8278a79480130c4ce2964812
Tea <icepie.dev@gmail.com>
2020-03-27 04:09:30 +0800

    update get_sha256 && login

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import hashlib
 
 # data
 url = "http://hmgr.sec.lit.edu.cn/wms/healthyLogin"
-username = "" #input your username
-password = 123456
+username = '' #input your username
+password = '' #input your password
 
 # sha256 for password
-def get_sha256(data):
-    s1 = hashlib.sha256()
-    s1.update(json.dumps(data).encode())
-    return s1.hexdigest()
+def get_sha256(password: str) -> str:
+    s = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    return s
 
 # login
 headers = {
@@ -26,8 +25,8 @@ data = {
 
 response = requests.post(url=url, data=json.dumps(data), headers=headers, verify=False)
 
-# print(response.json())
-if not 'True' in response.json():
+#print(response.json())
+if response.json()['code'] == 200:
     print("登陆成功!")
     print('学号: ' + response.json()['data']['teamNo'])
 else:
