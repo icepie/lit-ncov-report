27cf15c1a47c24268e876d6b59e211749231b2a4
Tea <icepie.dev@gmail.com>
2020-04-02 01:20:09 +0800

    fix tg push tag

--- src/push.py
+++ src/push.py
@@ -86,12 +86,11 @@ def tg_bot_run():
     
     bot = telebot.TeleBot(tgtoken)
 
-    desp = '#lnr ' + '\n'
-    desp += '-------------- 今日上报任务已完成! ------------' + '\n'
+    desp = '-------------- 今日上报任务已完成! ------------' + '\n'
 
     if get_value('table') == 1:
         desp += get_value('tbt')
-        desp = '<pre>' + desp + '</pre>'
+        desp = '#lnr ' + '\n' + '<pre>' + desp + '</pre>'
 
         tg_response = bot.send_message(chat_id=tgid, text=desp, parse_mode='HTML')
     else:
