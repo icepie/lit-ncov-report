67014ed3099bb96e122f7cb5e1beb698dbf46852
icepie <icepie.dev@gmail.com>
2021-01-17 22:39:51 +0800

    test 0.0.7

--- litncov/main.py
+++ litncov/main.py
@@ -1,13 +1,15 @@
 import click
- 
+
+
 @click.command()
 @click.option('--count', default=1, help='Number of greetings.')
 @click.option('--name', prompt='Your name',
-    help='The person to greet.')
+              help='The person to greet.')
 def hello(count, name):
- """Simple program that greets NAME for a total of COUNT times."""
- for x in range(count):
-  click.echo('Hello %s!' % name)
- 
-if __name__ == '__main__':
- hello()
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo('Hello %s!' % name)
+
+
+def main():
+    hello()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path as os_path
 from setuptools import setup
 
 setup(name='litncov',
-      version='0.0.6',
+      version='0.0.7',
       description='A ncov report library and tool for LIT(Luoyang Institute of Science and Technology)',
       url='http://github.com/icepie/lit-ncov-report',
       author='Tea',
@@ -14,7 +14,7 @@ setup(name='litncov',
       python_requires='>=3.6',
       entry_points={
           'console_scripts': [
-              'litncov = litncov:main',
+              'litncov = litncov.main:main',
           ],
       }
       )
