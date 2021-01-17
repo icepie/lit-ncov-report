0a04c6a6d4cdc8abea02c54aa7f0a5febbd57f72
icepie <icepie.dev@gmail.com>
2021-01-17 22:27:49 +0800

    test 0.0.4

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from os import path as os_path
 from setuptools import setup
 
 setup(name='litncov',
-      version='0.0.3',
+      version='0.0.4',
       description='A ncov report library and tool for LIT(Luoyang Institute of Science and Technology)',
       url='http://github.com/icepie/lit-ncov-report',
       author='Tea',
       author_email='icepie.dev@gmail.com',
       license='MIT',
       packages=['litncov'],
-      install_requires=['requests', 'gb2260'],
+      install_requires=['click', 'requests', 'gb2260'],
       python_requires='>=3.6')
