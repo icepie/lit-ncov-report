2ae7b8f9023aca79abc35a175b3acd2c2ae8d517
Tea <icepie.dev@gmail.com>
2021-05-15 10:33:42 +0800

    fix ua

--- litncov/user.py
+++ litncov/user.py
@@ -18,6 +18,7 @@ class litUesr:
         self.__headers = {
             "Connection": "keep-alive",
             "Content-Type": "application/json",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
         }
 
         # login the acconut
