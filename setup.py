6e93db8affc49e7a31391cee328779cf162a2cb8
Tea <icepie.dev@gmail.com>
2021-05-15 10:44:31 +0800

    ver 2.1.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="litncov",
-    version="2.1.5",
+    version="2.1.6",
     description="A ncov report library and tool for LIT(Luoyang Institute of Science and Technology)",
     long_description=long_description,
     long_description_content_type="text/markdown",
